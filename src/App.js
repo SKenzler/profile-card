8cedef8d58e02c5c5957f4eeeee3893b3f95420b
Shane Kenzler <104663659+SKenzler@users.noreply.github.com>
2022-11-28 22:04:25 +0800

    add styling to header, title and content. Add and style a button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import Card from './Components/Card';
 
 function App() {
   return (
 
     <div>
       
-      <card name="Shane Kenzler" job="React Frontend Developer" desc="I am an experienced graphic design, digital media and management professional with additional capabilities in front end development. I am organised and a problem solver with a highly client focused approach. I am seeking a position that will allow me to continue my professional development in a web development role." />
+      <Card name="Shane Kenzler" job="React Frontend Developer" desc="I am an experienced graphic design, digital media and management professional with additional capabilities in front end development. I am organised and a problem solver with a highly client focused approach. I am seeking a position that will allow me to continue my professional development in a web development role." />
 
     </div>
 
--- src/Components/card.js
+++ src/Components/card.js
@@ -1,28 +1,37 @@
 import React from 'react';
-import profile from './Portfolio_Hero_Image_sml.jpg';
+import '../App.css';
+import profile from '../Portfolio_Hero_Image_sml.jpg';
 
-export default function card (props) {
+export default function Card (props) {
 
     return (
        
-        <div className='card'>
+        <div className="card">
 
-            <div className='image-container'>
+            <div className="image-container">
                 <img src={ profile } className='profile-picture' alt="Profile" />
             </div>
 
-            <div className="card-header">
-                <h1> { props.name } </h1>
-            </div>
+            <div className="hero-container">
 
-            <div className="card-title">
-                <h2> { props.job } </h2>
-            </div>
+                <div className="card-header">
+                    <h1> { props.name } </h1>
+                </div>
 
-            <div className='card-content'>
-                <p> { props.desc } </p>
+                <div className="card-title">
+                    <h2> { props.job } </h2>
+                </div>
+
+                <div className="card-content">
+                    <p> { props.desc } </p>
+                </div>
+
+                <div className="">
+                    <button type="button" className="btn-find">Find out more</button>
+                </div>
+
             </div>
             
         </div>
-    )
+    );
 }
