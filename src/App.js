eb42eb0ee566af2bed4bcc89797da6d2b34384e2
Shane Kenzler <104663659+SKenzler@users.noreply.github.com>
2022-12-01 18:23:03 +0800

    final change before inital delpoyment

--- src/App.js
+++ src/App.js
@@ -6,7 +6,18 @@ function App() {
 
     <div>
       
-      <Card name="Shane Kenzler" job="React Frontend Developer" desc="I am an experienced graphic design, digital media and management professional with additional capabilities in front end development. I am organised and a problem solver with a highly client focused approach. I am seeking a position that will allow me to continue my professional development in a web development role." />
+      <Card 
+      name="Shane Kenzler" 
+      job="React Frontend Developer" 
+      desc="I am an experienced graphic 
+      design, digital media and management 
+      professional with additional 
+      capabilities in frontend development. 
+      I am organised and a problem solver 
+      with a highly client focused approach. 
+      I am seeking a position that will allow 
+      me to continue my professional development 
+      in a web development role." />
 
     </div>
 
